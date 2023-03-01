196ef597a2d721ba9d3f32ff15898228eddd153b
felipezzz1 <felipezorzo78@gmail.com>
2023-03-01 14:08:18 -0300

    Parte  3 - Customers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import passwordRouter from "@modules/users/routes/password.routes";
 import profileRouter from "@modules/users/routes/profile.routes";
 import sessionsRouter from "@modules/users/routes/sessions.routes";
 import { Router } from "express";
+import customerRouter from "@modules/customers/routes/customer.routes";
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customerRouter);
 
 export default routes;
